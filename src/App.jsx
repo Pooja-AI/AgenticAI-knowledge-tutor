d099b207316f485a650fcd2d37350b46b0a7fe69
Pooja Sunkara <sunkara.pooja226@gmail.com>
2026-07-18 18:42:42 +0530

    update code

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { HashRouter, Routes, Route } from "react-router-dom";
 import AgenticAICookbook from "./pages/AgenticAICookbook";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <HashRouter>
+      <Navbar />
       <Routes>
         <Route path="/" element={<AgenticAICookbook />} />
       </Routes>
